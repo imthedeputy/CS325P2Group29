86f9e651dba0a302a7cfc0f7e2327e9a63b36fcb
eligoodwin <goodwiel@oregonstate.edu>
2017-04-30 14:58:44 -0700

    Add files via upload

--- helperFunctions.hpp
+++ helperFunctions.hpp
@@ -18,12 +18,12 @@
 #include <string>
 #include "changeInfo.hpp"
 #include "FileInAndOut.hpp"
-
+#include <climits>
 void algo1(struct changeInfo& changeData);
 void algo2(struct changeInfo& changeData);
 void algo3(struct changeInfo& changeData);
 
-
+void makeAlgo1TestSet(std::vector<struct changeInfo>& changeTestSet, std::vector<int> denomInput);
 
 
 
