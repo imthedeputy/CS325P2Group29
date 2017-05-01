c6848bf82f96430784b6b61eeddda6147ec5bf31
bentate92 <tateb@onid.oregonstate.edu>
2017-04-30 17:55:21 -0700

    Updating helperFunctions.cpp algo3() and changedp()

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -85,6 +85,57 @@ void changegreedy(int v[], int c[], int a, int length) {
     }
 }
 
+void changedp(int V[], int C[], int length, int A, int m) {
+    int cases[A + 1]; 
+    int result[A + 1];
+
+    //Base case
+    cases[0] = 0;
+
+    //Initialize all elements in min as maximum int
+    for(int i = 1; i <= A; i++) {
+        cases[i] = INT_MAX;
+        result[i] = -1;
+    }
+
+    //Compute minimum coins for V = 1...V
+    for(int i = 1; i <= A; i++) {
+        //Loop through denominations that are <= i
+        for(int j = length - 1; j >= 0; j--) {
+            if(V[j] <= i) {
+                int res = cases[i - V[j]];
+
+                if(res != INT_MAX && res + 1 < cases[i]) {
+                    cases[i] = res + 1;
+                    result[i] = j;
+                }
+            }
+        }
+    }
+
+    int res_itr = A;
+
+    //Remove next 2 comments to print coins used
+//  cout << "The coins are: ";
+    while(res_itr != 0) {
+        int k = result[res_itr];
+//      cout << V[k] << " ";
+        C[k]++;
+        res_itr = res_itr - V[k];
+    }
+    cout << endl;
+
+    m = cases[A];
+
+    cout << "Min coins = " << cases[A] << endl;
+    cout << "C = ";
+    for(int i = 0; i < length; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    return;
+}
 
 void algo1(struct changeInfo& changeData){
     //get the size of the denoms vector
@@ -139,9 +190,26 @@ void algo2(struct changeInfo& changeData) {
     delete []c;
 }
 
-void algo3(struct chnageInfo& changeData){
-    cout << "do something " << endl;
+void algo3(struct changeInfo& changeData){
+    int length = (int)changeData.denoms.size();
+    int* V = new int[length];
+    int* C = new int[length]; 
+
+	for(int i = 0; i < length; i++) {
+		V[i] = changeData.denoms[i];
+	}
     
+    auto start = chrono::high_resolution_clock::now();
+    changedp(V, C, length, changeData.amount, changeData.coinsUsed); 
+    auto elapsed = chrono::high_resolution_clock::now() - start;
+    changeData.runtime = chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+
+    for(int i = 0; i < length; i++) {
+        changeData.denomsUsed.push_back(C[i]);
+    }
+
+	delete []V;
+	delete []C;
 }
 
 
